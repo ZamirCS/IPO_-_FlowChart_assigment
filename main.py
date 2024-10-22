2288e8b502d87ddff8f34453e4ea53e0f6b7aee2
Zamir <zamir357@icloud.com>
2024-10-21 23:32:53 -0400

    Assigment

---
+++ main.py
@@ -0,0 +1,13 @@
+movie_cost = 8
+harry_money = 7
+judy_money = 12
+
+if harry_money >= movie_cost:
+    print("Harry can see the movie.")
+else:
+    print("Harry will play at the arcade.")
+
+if judy_money >= movie_cost:
+    print("Judy can see the movie.")
+else:
+    print("Judy will play at the arcade.")
